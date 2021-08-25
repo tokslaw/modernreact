349e8b8a0f41ba6c0bf6622d7f139ccf67eef736
tokslaw <tokslaw@chiefsoft.com>
2021-08-25 09:46:21 -0400

    now functional components updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "./layout/Navbar";
+//import UserItem from './users/UserItem';
+import Users from "./users/Users";
+import Contact from "./Pages/Contact";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 class App extends Component{
   render(){
-    return(
-      <div className='App'>
-
-        <Navbar />
-
-      </div>
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Users />
+            </div>
+        </div>
+        <Route path="/contact" component={Contact} />
+      </Router>
     );
   }
 }
